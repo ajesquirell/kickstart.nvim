3719d20c12e32d0ed23f7a407df2784e1ec505bf
Austin Esquirell <aesquirell@innerspec.com>
2026-03-09 19:01:43 -0400

    Add Custom Keymaps section to init.lua. Try using jj to return to normal mode

--- init.lua
+++ init.lua
@@ -218,6 +218,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ Custom Keymaps ]]
+vim.keymap.set('i', 'jj', '<ESC>', { desc = 'Return to normal mode' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
