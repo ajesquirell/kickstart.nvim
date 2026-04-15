e6d6f945182512e5606ccb0eeee2f52c087c0eed
Austin Esquirell <aesquirell@innerspec.com>
2026-04-15 11:28:20 -0400

    Add mini.animate plugin, with mouse detection (from LazyVim config)

--- init.lua
+++ init.lua
@@ -886,7 +886,41 @@ require('lazy').setup({
       --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
   },
+  {
+    'nvim-mini/mini.animate',
+    event = 'VeryLazy',
+    opts = function(_, opts)
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs { 'Up', 'Down' } do
+        local key = '<ScrollWheel' .. scroll .. '>'
+        vim.keymap.set({ '', 'i' }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
 
+      local animate = require 'mini.animate'
+      return vim.tbl_deep_extend('force', opts, {
+        cursor = { timing = animate.gen_timing.linear { duration = 50, unit = 'total' } },
+        resize = {
+          timing = animate.gen_timing.linear { duration = 50, unit = 'total' },
+        },
+        scroll = {
+          timing = animate.gen_timing.linear { duration = 150, unit = 'total' },
+          subscroll = animate.gen_subscroll.equal {
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          },
+        },
+      })
+    end,
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
